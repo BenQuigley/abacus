22e6a5079c6266a8b1d91324f1fa3f3779b35f9b
Ben Quigley <ben.quigley.all@gmail.com>
2023-09-03 21:55:25 -0400

    Minor updates

--- abacus.py
+++ abacus.py
@@ -1,62 +1,70 @@
-from __future__ import print_function
+from dataclasses import dataclass
+from typing import Optional
 
-class abacus:
+
+@dataclass
+class Abacus:
     # Reference: https://www.youtube.com/watch?v=CvsnftXXKdw
-    def __init__(self):
-        self.value    = 0.0
-        self.places   = 6
-        self.decimals = 2
-        self.length   = self.places + self.decimals
-    
-    def display(self):
-        self.string = str(int(self.value*100)).zfill(9)
-        self.board = ['','','','','','','','','','']
+    value: float = 0.0
+    places: int = 6
+    decimals: int = 2
+    board: Optional[list[int]] = None
 
-        print('Displaying abacus showing value:', self.value)
+    @property
+    def length(self):
+        return self.places + self.decimals
+
+    def display(self):
+        self.string = str(int(self.value * 100)).zfill(9)
+        self.board = ["", "", "", "", "", "", "", "", "", ""]
+
+        print("Displaying abacus showing value:", self.value)
         place = 0
-        stop = len(self.string) # min of 9 because of zfill
-        while place <  stop:
+        stop = len(self.string)  # min of 9 because of zfill
+        while place < stop:
             digitvalue = int(self.string[place])
             spacing = False
             if len(self.board[0]) % 2 != 0:
                 spacing = True
             for i, row in enumerate(self.board):
                 if i in [0, 3, 9]:
-                    self.board[i] += '='
+                    self.board[i] += "="
                 elif stop - place == 2 and spacing:
-                    self.board[i] += '|'
+                    self.board[i] += "|"
                 elif spacing:
-                    self.board[i] += ' '
+                    self.board[i] += " "
                 else:
-                    heavennum = int(digitvalue/5)
+                    heavennum = int(digitvalue / 5)
                     earthnum = digitvalue % 5
                     # "heaven"
                     if i <= 2:
-                        if heavennum == i-1:
-                            self.board[i] += '-'
+                        if heavennum == i - 1:
+                            self.board[i] += "-"
                         else:
-                            self.board[i] += ' '
+                            self.board[i] += " "
                     # "earth"
                     else:
                         if earthnum == i - 4:
-                            self.board[i] += ' '
+                            self.board[i] += " "
                         else:
-                            self.board[i] += '-'
+                            self.board[i] += "-"
             if not spacing:
                 place += 1
 
-        print('\n'.join(self.board),'\n')
+        print("\n".join(self.board), "\n")
+
 
 def main():
-    test = abacus()
-    prompt = 'Enter a number to display on the pretty abacus here:\n>'
+    abacus = Abacus()
+    prompt = "Enter a number to display on the pretty abacus here:\n> "
     while True:
         try:
-            test.value += float(input(prompt))
-            test.display()
+            abacus.value += float(input(prompt))
+            abacus.display()
         except ValueError:
-            print('That\'s not a number.')
-        prompt = 'Great job! Now enter a number to add or subtract:\n>'
+            print("That's not a number.")
+        prompt = "Enter a number to add or subtract:\n> "
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
